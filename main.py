702f39847922eb2d7c96f3a66a2623013c0d3272
RibbonEyes1 <Joceline.marinhernandez25@ycdsbk1.ca>
2024-06-03 10:58:13 -0400

    Starting with background and temporary sprites

--- main.py
+++ main.py
@@ -13,12 +13,22 @@ pygame.init()
 # Set up the display
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pygame Window")
+pygame.display.set_caption("Last Stand")
 
 # Define colours
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+#get background
+background = pygame.image.load("First_Spawn.png")
+background = pygame.transform.scale(background,(WIDTH,HEIGHT))
+
+temp = pygame.image.load("PlaceHolder.png")
+temp_rect = temp.get_rect(center=(WIDTH-10,HEIGHT//1.5))
+temp = pygame.transform.scale(temp,(270,HEIGHT/2))
+#turn into list
+temp_rect.x = 380
+temp_rect.y = 260
 # Initialize more variables and states below
 #
 
@@ -35,8 +45,10 @@ while running:
     # Update game state
 
     # Draw graphics and output
-    screen.fill(WHITE)
+    #screen.fill(WHITE)
 
+    screen.blit(background,(1,0))
+    screen.blit(temp,temp_rect)
     # Update display
     pygame.display.flip()
     clock.tick(30)  # Limit frame rate to 30 fps
