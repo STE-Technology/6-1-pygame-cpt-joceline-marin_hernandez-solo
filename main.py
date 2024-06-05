0a4db279ab59833fb0f11b7f2483578afe867b4a
RibbonEyes1 <Joceline.marinhernandez25@ycdsbk1.ca>
2024-06-05 10:57:58 -0400

    Am going to try doig this at home again.Thank you for showing me the collisions more extensively

--- main.py
+++ main.py
@@ -24,19 +24,22 @@ background = pygame.image.load("First_Spawn.png")
 background = pygame.transform.scale(background,(WIDTH,HEIGHT))
 
 temp = pygame.image.load("PlaceHolder.png")
-temp_rect = temp.get_rect(center=(WIDTH-120,HEIGHT//5.5))
-temp = pygame.transform.scale(temp,(270,HEIGHT/2))
+#temp_rect = temp.get_rect(center=(WIDTH,HEIGHT//5.5))
+temp_rect = temp.get_rect()
+temp = pygame.transform.scale(temp,(90,HEIGHT/5.5))
 #turn into list
 temp_rect.x = 380
-temp_rect.y = 170
-temp_rect_speed=16
+temp_rect.y = 220
+temp_rect_speed=10
 
 enemy=pygame.image.load("Temp_Enemy.png")
-enemy_rect = enemy.get_rect(center=(WIDTH-20,HEIGHT//1.3))
-enemy= pygame.transform.scale(enemy,(300,HEIGHT/2))
+#enemy_rect = enemy.get_rect(center=(WIDTH-20,HEIGHT//1.3))
+enemy_rect= enemy.get_rect()
+enemy= pygame.transform.scale(enemy,(120,HEIGHT/4.5))
 
 enemy_rect.x=-120
-enemy_rect.y=170
+enemy_rect.y=200
+enemy_rect_speed: 8
 
 fireball=pygame.image.load("temp_fireball.png")
 fireball_rect = fireball.get_rect(center=(10,HEIGHT//1.6))
@@ -46,6 +49,12 @@ fireball_rect.y = temp_rect.y + 151
 
 # Initialize more variables and states below
 #
+
+#### TESTING
+# if fireball_rect.colliderect(enemy_rect):
+#     # do something
+###
+
 
 # Define main loop
 running = True
@@ -68,25 +77,34 @@ while running:
 
     input=pygame.key.get_pressed()
 
+    #if enemy_rect.colliderect(temp_rect):
+        
 
+
+   # if enemy_rect <= temp_rect:
+    #    enemy_rect.x += 4
+   # elif enemy_rect >= temp_rect:
+   #     enemy_rect.x -= 4
 
     if input[pygame.K_LEFT]:
         temp_rect.x -= temp_rect_speed
-
     if input[pygame.K_RIGHT]:
         temp_rect.x += temp_rect_speed
+
+    
 
     if input[pygame.K_1]:
         screen.blit(fireball,fireball_rect)
         fireball_rect.x = temp_rect.x + 10
-        fireball_rect.x -= 10
+        fireball_rect.y = temp_rect.y +2
+       # while fireball_rect not 
       
         
        # while fireball_rect.x >= -150:
         # fireball_rect.x -= 2
-        print (temp_rect)
+        print (enemy_rect.x)
        
-        
+ 
 
     # Update display
     pygame.display.flip()
